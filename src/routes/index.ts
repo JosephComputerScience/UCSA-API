90381e2a4df6a7efc516204a7a7ea9d51792fcb3
dryomas <shintodr@gmail.com>
2022-07-01 11:08:33 -0500

    making new branch and committing match v5 services

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,9 @@
 import Router from 'express-promise-router';
 // routes
 import { summonerRouter } from './summoner';
+import { matchRouter } from './match';
 
 export const v1Router = Router();
 
 v1Router.use(summonerRouter);
+v1Router.use(matchRouter);
