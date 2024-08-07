a6a8cc93ee1fb28d26c8ce6a89a6552c1a368d13
Joseph Nguyen <jonguyen741@gmail.com>
2024-08-06 23:38:36 -0500

    add user dao,repository and summoner dao and repository (#5)

--- src/controllers/user.controller.ts
+++
@@ -1,5 +0,0 @@
-import { fastify, FastifyReply, FastifyRequest } from 'fastify';
-
-export const getIndex = (req: FastifyRequest, reply: FastifyReply) => {
-  return reply.send({ success: true });
-};
