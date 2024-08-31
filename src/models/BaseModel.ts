614036d283e2bc6dbf8bb3329be2c731079a13fb
Joseph <jonguyen741@gmail.com>
2024-08-31 11:54:17 -0500

    remove super call

--- src/models/BaseModel.ts
+++
@@ -1,3 +0,0 @@
-export abstract class BaseModel<T> {
-  abstract update: (data: T) => void;
-}
--- src/models/Summoner.ts
+++ src/models/Summoner.ts
@@ -1,15 +1,15 @@
 import { BaseModel } from './BaseModel';
 
-export class Summoner extends BaseModel<Summoner> {
-  private _puuid: string;
-  private _summonerName: string;
-  private _tagLine: string;
-  private _accountId: string;
-  private _summonerId: string;
-  private _summonerLevel: number;
-  private _profileIconId: number;
-  private _revisionDate: Date;
-  private _updatedAt: Date;
+export class Summoner {
+  puuid: string;
+  summonerName: string;
+  tagLine: string;
+  accountId: string;
+  summonerId: string;
+  summonerLevel: number;
+  profileIconId: number;
+  revisionDate: Date;
+  updatedAt: Date;
 
   constructor(
     puuid: string,
@@ -22,73 +22,14 @@ export class Summoner extends BaseModel<Summoner> {
     revisionDate: Date,
     updatedAt: Date
   ) {
-    super();
-    this._puuid = puuid;
-    this._summonerName = summonerName;
-    this._tagLine = tagLine;
-    this._accountId = accountId;
-    this._summonerId = summonerId;
-    this._summonerLevel = summonerLevel;
-    this._profileIconId = profileIconId;
-    this._revisionDate = revisionDate;
-    this._updatedAt = updatedAt;
+    this.puuid = puuid;
+    this.summonerName = summonerName;
+    this.tagLine = tagLine;
+    this.accountId = accountId;
+    this.summonerId = summonerId;
+    this.summonerLevel = summonerLevel;
+    this.profileIconId = profileIconId;
+    this.revisionDate = revisionDate;
+    this.updatedAt = updatedAt;
   }
-
-  get summonerName(): string {
-    return this._summonerName;
-  }
-
-  get tagLine(): string {
-    return this._tagLine;
-  }
-
-  get accountId(): string {
-    return this._accountId;
-  }
-
-  get puuid(): string {
-    return this._puuid;
-  }
-
-  get summonerId(): string {
-    return this._summonerId;
-  }
-
-  get summonerLevel(): number {
-    return this._summonerLevel;
-  }
-
-  get profileIconId(): number {
-    return this._profileIconId;
-  }
-
-  get revisionDate(): Date {
-    return this._revisionDate;
-  }
-
-  get updatedAt() {
-    return this._updatedAt;
-  }
-
-  update = (summoner: Summoner) => {
-    const {
-      puuid,
-      summonerName,
-      tagLine,
-      accountId,
-      summonerId,
-      summonerLevel,
-      profileIconId,
-      revisionDate,
-    } = summoner;
-    this._puuid = puuid;
-    this._summonerName = summonerName;
-    this._tagLine = tagLine;
-    this._accountId = accountId;
-    this._summonerId = summonerId;
-    this._summonerLevel = summonerLevel;
-    this._profileIconId = profileIconId;
-    this._revisionDate = revisionDate;
-    this._updatedAt = new Date();
-  };
 }
